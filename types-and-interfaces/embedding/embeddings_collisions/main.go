c560370f89ccca3bc9bbe72d3fc1ba4eed6d8b44
quicksloth8 <quicksloth8@gmail.com>
2021-10-31 15:51:56 +0200

    embeddings_collisions - added example of collision with embedding structure

--- types-and-interfaces/embedding/embeddings_collisions/main.go
+++ types-and-interfaces/embedding/embeddings_collisions/main.go
@@ -20,9 +20,15 @@ import "fmt"
 
 func main() {
 	a := &Struct3{
-		Struct1: Struct1{"A1", 1},
-		Struct2: Struct2{"A2", 2},
+		Struct1: Struct1{"A1", 1, "1111"},
+		Struct2: Struct2{"A2", 2, "2222"},
+		D:       "3333",
 	}
+
+	// collision with embedding struct - embedding struct's field/method overwrites others
+	fmt.Println(a.D)
+	a.common()
+
 	fmt.Println(a.B)
 	fmt.Println(a.C)
 	//fmt.Println(a.A) // compilation error - ambiguous selector a.A
@@ -40,30 +46,45 @@ func main() {
 type Struct1 struct {
 	A string
 	B int
+	D string
 }
 
 func (s Struct1) foo() {
-	fmt.Println("Struct1's foo()")
+	fmt.Println("Struct1 foo()")
 }
 
 func (s Struct1) bar1() {
 	fmt.Println("bar1()")
 }
 
+func (s Struct1) common() {
+	fmt.Println("Struct1 - common()")
+}
+
 type Struct2 struct {
 	A string
 	C int
+	D string
 }
 
 func (s Struct2) foo() {
-	fmt.Println("Struct2's foo()")
+	fmt.Println("Struct2 foo()")
 }
 
 func (s Struct2) bar2() {
 	fmt.Println("bar2()")
 }
 
+func (s Struct2) common() {
+	fmt.Println("Struct2 - common()")
+}
+
 type Struct3 struct {
 	Struct1
 	Struct2
+	D string
+}
+
+func (s Struct3) common() {
+	fmt.Println("Struct3 - common()")
 }
